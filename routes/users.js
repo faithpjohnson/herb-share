3b16813b112fc16d19079be756379b059631adb6
Faith Johnson <faithpjohnson@gmail.com>
2022-01-10 11:23:02 -0700

    set up oauth

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,17 @@
-var router = require('express').Router();
-
+var router = require("express").Router();
+const usersCtrl = require("../controllers/users");
 
 // GET /students
-// router.get('/students', studentsCtrl.index);
-
-
+router.get("/users", usersCtrl.index);
 
 // Authorizing the user to use a route
 // probably only want to use this on
 // post, put or delete routes
 function isLoggedIn(req, res, next) {
-	// req.isAuthenticated() this is given to us by passport
-	// it returns true or false
-	if ( req.isAuthenticated() ) return next(); // next() go to the next function in middleware, above situation studentsCtrl.addFact
-	res.redirect('/auth/google'); // redirect them to login
+  // req.isAuthenticated() this is given to us by passport
+  // it returns true or false
+  if (req.isAuthenticated()) return next(); // next() go to the next function in middleware, above situation studentsCtrl.addFact
+  res.redirect("/auth/google"); // redirect them to login
 }
-
-
 
 module.exports = router;
